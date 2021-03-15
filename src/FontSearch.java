1f0dae767dc0d36cb5f1d650ccf95c415e3e9186
Mttblue2 <42953039+Mttblue2@users.noreply.github.com>
2021-03-14 22:35:53 -0400

    Update FontSearch.java
    Adding font sorting to finding installed fonts. broke up methods

--- src/FontSearch.java
+++ src/FontSearch.java
@@ -4,6 +4,9 @@ import java.awt.GraphicsEnvironment;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 import com.aspose.psd.Image;
@@ -15,32 +18,21 @@ public class FontSearch
 
 	public static void main(String[] args) throws FontFormatException, IOException
 	{
-		String homePath = System.getProperty("user.home");
-		String fontDirectory = homePath + "\\AppData\\Local\\Microsoft\\Windows\\Fonts";
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("1: get PSD fonts \n2: get installed fonts");
+		String action = keyboard.next();	
 		
-		File fontScanner = new File(fontDirectory);
-		
-		File[] fontList = fontScanner.listFiles();
-		ArrayList<Font> fonts = new ArrayList<Font>();
-		
-		for (int x = 0; x < fontList.length; x++)
-		{
-			if (!fontList[x].isDirectory())
-			{
-				try
-				{
-					fonts.add(Font.createFont(Font.TRUETYPE_FONT, fontList[x]));
-					System.out.println(fonts.get(fonts.size()-1).getFontName());
-				}
-				catch (Exception e)
-				{
-					int y = fontList[x].toString().lastIndexOf("\\");
-					System.out.println("Font " + fontList[x].toString().substring(y+1) + " had an error.");
-				}
-			}
-		}
-		
-		/*File dir = new File(System.getProperty("user.dir"));
+		if (action.equals("1"))
+			getPSDFonts();
+		else if (action.equals("2"))
+			getInstalledFonts();
+
+		keyboard.close();
+	}
+
+	public static void getPSDFonts()
+	{
+		File dir = new File(System.getProperty("user.dir"));
 		PsdImage psdImage;
 		ArrayList<String> list = new ArrayList<String>();
 
@@ -54,7 +46,7 @@ public class FontSearch
 
 				try
 				{
-				System.out.println(array[x].getName());
+					System.out.println(array[x].getName());
 
 					psdImage = (PsdImage) Image.load(array[x].toString());
 
@@ -84,8 +76,48 @@ public class FontSearch
 				}
 
 			}
-		}*/
-
+		}
 	}
 
+	public static void getInstalledFonts()
+	{
+		String homePath = System.getProperty("user.home");
+		String fontDirectory = homePath + "\\AppData\\Local\\Microsoft\\Windows\\Fonts";
+
+		File fontScanner = new File(fontDirectory);
+
+		File[] fontList = fontScanner.listFiles();
+		ArrayList<Font> fonts = new ArrayList<Font>();
+
+		for (int x = 0; x < fontList.length; x++)
+		{
+			if (!fontList[x].isDirectory())
+			{
+				try
+				{
+					fonts.add(Font.createFont(Font.TRUETYPE_FONT, fontList[x]));
+				} catch (Exception e)
+				{
+					int y = fontList[x].toString().lastIndexOf("\\");
+					System.out.println("Font " + fontList[x].toString().substring(y + 1) + " had an error.");
+				}
+			}
+		}
+		System.out.println("");
+
+		Collections.sort(fonts, new fontCompare());
+
+		for (int x = 0; x < fonts.size(); x++)
+		{
+			System.out.println(fonts.get(x).getFontName());
+		}
+	}
+}
+
+class fontCompare implements Comparator<Font>
+{
+	public int compare(Font a, Font b)
+	{
+		return a.getFontName().toLowerCase().compareTo(b.getFontName().toLowerCase());
+	}
 }
